ce60c92fb2a552afe44d48668f39a7e234ba083d
Dmitry <dmitry.sh23@gmail.com>
2023-11-23 12:00:24 +0300

    Home Work 2

---
+++ Assets/CharacterExample/Scripts/Character/StateMachine/States/Grounded/FastRunningState.cs
@@ -0,0 +1,36 @@
+﻿public class FastRunningState : GroundedState
+{
+    private readonly FastRunningStateConfig _config;
+
+    public FastRunningState(IStateSwitcher stateSwitcher, StateMachineData data, Character character) : base(stateSwitcher, data, character)
+        => _config = character.Config.FastRunningStateConfig;
+
+    public override void Enter()
+    {
+        base.Enter();
+
+        View.StartFastRunning();
+        Data.Speed = _config.Speed;
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+
+        View.StopFastRunning();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if (IsHorizontalInputZero())
+            StateSwitcher.SwitchState<IdlingState>();
+
+        if (IsRunKeyHolded() == false)
+            StateSwitcher.SwitchState<WalkingState>();
+
+        if (IsRunKeyHolded() && IsFastRunKeyHolded() == false)
+            StateSwitcher.SwitchState<RunningState>();
+    }
+}
